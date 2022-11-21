55446c3ee5b22e91aff250f315fd30242d7cbbc4
cake-dev <developer.montana@gmail.com>
2022-11-21 10:48:38 -0700

    merge CRUD and UI

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -3,3 +3,30 @@ function hideError() {
   document.getElementsByClassName("errorlist")[0].style.display = "none";
 }
 // hideError();
+
+// move post form area with scroll keeping in same alignment using jquery
+// $(document).ready(function () {
+//   var nav = $('#post_block');
+//   var navHomeY = nav.offset().top;
+//   var isFixed = false;
+//   var $w = $(window);
+//   $w.scroll(function () {
+//     var scrollTop = $w.scrollTop();
+//     var shouldBeFixed = scrollTop > navHomeY;
+//     if (shouldBeFixed && !isFixed) {
+//       nav.css({
+//         position: 'fixed',
+//         top: 0,
+//         left: nav.offset().left,
+//         width: nav.width()
+//       });
+//       isFixed = true;
+//     }
+//     else if (!shouldBeFixed && isFixed) {
+//       nav.css({
+//         position: 'static'
+//       });
+//       isFixed = false;
+//     }
+//   });
+// });
