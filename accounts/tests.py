bb5f66b5936fd8e68c7144473fec61c957d7e0b7
cake-dev <developer.montana@gmail.com>
2022-12-08 11:38:13 -0700

    fix tests

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,6 @@
 from django.test import TestCase
+from django.test import SimpleTestCase
+from django.urls import reverse
+from django.contrib.auth import get_user_model
 
 # Create your tests here.
