412a84f0f83e887180579459207e597e4fbf1aa3
cake-dev <developer.montana@gmail.com>
2022-12-08 12:42:22 -0700

    add ststic and media root to urlpatterns

--- posty/urls.py
+++ posty/urls.py
@@ -26,5 +26,5 @@ urlpatterns = [
     path("profile/<int:pk>/", profile, name="profile"),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- social/urls.py
+++ social/urls.py
@@ -15,6 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path("", include("posty.urls")),
@@ -25,3 +28,5 @@ urlpatterns = [
     # path(r'^admin/', admin.site.urls),
     # path(r'^', include('posty.urls')),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
